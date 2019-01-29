43aa5e2ad8e601407bfc199d5d3b37e7f1200531
tomrains <tomrainswrites@gmail.com>
2019-01-28 21:10:01 -0600

    Adding in oneTurn function

--- projects/conways-game-of-life/conway.js
+++ projects/conways-game-of-life/conway.js
@@ -3,3 +3,42 @@ p.onclick = lifeAndDeath(this)
 function lifeAndDeath(el) {
   el.classList.toggle("alive");
 }
+
+//the oneTurn function should execute when the user clicks the Go! button (so could probably mimick the current onclick function)
+function oneTurn {
+  var classes = []; 
+  var newClasses = [];
+  var squares = document.getElementsbyTagName('p');
+  for (i=0; i<squares.length; i++) {
+    classes[i] = squares[i].class;
+  }
+  for (z=0; z<classes.length; z++) {
+    if (z == 0) {
+      if (classes[z+1] == "alive") {
+        newClasses[z] = "alive";
+      }
+      else {
+        newClasses[z] = "dead";
+      }
+    } // last curly for if z == 0
+    else if (z > 0 && z < 7) {
+      if (classes[z - 1] == classes[z + 1]) {
+        newClasses[z] = "dead";
+      }
+      else {
+        newClasses[z] = "alive";
+      }
+    } // last curly for else if
+    else { //if z == 7
+      if (classes[z - 1] == "alive") {
+        newClasses[z] = "alive";
+      }
+      else {
+        newClasses[z] = "dead";
+      }
+    }
+    //now let's change all the styles!
+    for (i=0; i<squares.length; i++) {
+      squares[i].class = newClasses[i].class;
+  }
+  } //ending bracket for oneTurn function
