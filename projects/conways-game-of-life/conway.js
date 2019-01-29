6aebb1c054053c07c713a35886563f03ec76a308
tomrains <tomrainswrites@gmail.com>
2019-01-28 18:25:32 -0600

    Write separate id functions for each square
    I know this is not the optimal way to do this, but I haven't been able to figure out how to select JUST the element that's been clicked and toggle JUST its class. So I'm going to do this for now so I can move forward.

--- projects/conways-game-of-life/conway.js
+++ projects/conways-game-of-life/conway.js
@@ -1,4 +1,39 @@
-function lifeAndDeath() {
+function lifeAndDeath1() {
   var element = document.getElementById("square1");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath2() {
+  var element = document.getElementById("square2");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath3() {
+  var element = document.getElementById("square3");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath4() {
+  var element = document.getElementById("square4");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath5() {
+  var element = document.getElementById("square5");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath6() {
+  var element = document.getElementById("square6");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath7() {
+  var element = document.getElementById("square7");
+  element.classList.toggle("alive");
+}
+
+function lifeAndDeath8() {
+  var element = document.getElementById("square8");
   element.classList.toggle("alive");
 }
