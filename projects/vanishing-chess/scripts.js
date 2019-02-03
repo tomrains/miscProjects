beb40186e025ffaeea34cc981f52ff1b04a1d03c
tomrains <tomrainswrites@gmail.com>
2019-02-02 21:31:56 -0600

    add in all piece functions

--- projects/vanishing-chess/scripts.js
+++ projects/vanishing-chess/scripts.js
@@ -7,7 +7,21 @@ var chess_board = [["", "", "", "", "", "", "", ""],
                    ["", "", "", "", "", "", "", ""], 
                    ["", "", "", "", "", "", "", ""]]; 
 
+//All black pieces
 var blackRook = '<img src="images/black-rook.png">';
+var blackKnight = '<img src="images/black-knight.png">';
+var blackBishop = '<img src="images/black-bishop.png">';
+var blackQueen = '<img src="images/black-queen.png">';
+var blackKing = '<img src="images/black-king.png">';
+var blackPawn = '<img src="images/black-pawn.png">';
+
+//All white pieces
+var whiteRook = '<img src="images/white-rook.png">';
+var whiteKnight = '<img src="images/white-knight.png">';
+var whiteBishop = '<img src="images/white-bishop.png">';
+var whiteQueen = '<img src="images/white-queen.png">';
+var whiteKing = '<img src="images/white-king.png">';
+var whitePawn = '<img src="images/white-pawn.png">';
 
 function setBoard() {
   document.getElementById("1").innerHTML = blackRook;
