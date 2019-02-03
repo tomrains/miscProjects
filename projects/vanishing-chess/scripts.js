ddad71b858039768573ee25e27cdb3ee3187c9e8
tomrains <tomrainswrites@gmail.com>
2019-02-03 14:44:19 -0600

    Add selected and el to carry over to new function

--- projects/vanishing-chess/scripts.js
+++ projects/vanishing-chess/scripts.js
@@ -99,7 +99,7 @@ function allowMove(el) {
   var selected = document.querySelector("td.selected");
   //if pawn, run the movePawn function
   if (selected.innerHTML == whitePawn || selected.innerHTML == blackPawn) {
-    movePawn();
+    movePawn(selected, el);
   }
   //grab piece inside square
   var piece = selected.innerHTML;
