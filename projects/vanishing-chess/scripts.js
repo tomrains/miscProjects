b2f498c123912f86d5e640ba7dd5a21e4c97a24e
tomrains <tomrainswrites@gmail.com>
2019-02-03 16:38:25 -0600

    Disallowing blocked pawns to move forward
    This has one issue. It doesn't allow pawns to move to occupied spaces. This of course at one point will need to be changed - when pawns attack.

--- projects/vanishing-chess/scripts.js
+++ projects/vanishing-chess/scripts.js
@@ -61,8 +61,12 @@ function setBoard() {
 }
 
 function movePawn(selected, el) {
+  //disallow pawns with pieces directly in front of them to move
+  if (el.innerHTML != null) {
+    move = false;
+    return;
 //allow black pawn to move forward 1 or 2 squares if on opening position
-  if (selected.innerHTML == blackPawn && selected.id >= 9 && selected.id <= 16) {
+  else if (selected.innerHTML == blackPawn && selected.id >= 9 && selected.id <= 16) {
     if (el.id - selected.id != 8 && el.id - selected.id != 16) {
       move = false;
       return;
