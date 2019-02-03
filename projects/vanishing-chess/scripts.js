91fea2ae44f8922e194ba7f92721b8cb7cbe3429
tomrains <tomrainswrites@gmail.com>
2019-02-02 23:04:29 -0600

    check if array index undefined

--- projects/vanishing-chess/scripts.js
+++ projects/vanishing-chess/scripts.js
@@ -60,7 +60,7 @@ function setBoard() {
 
 function selectedPiece(el) {
   var selected = document.getElementsByClassName("selected");
-  if (selected !== undefined) {
+  if (typeof selected[0] == "undefined") {
     selected.classList.toggle("selected");
   }
   el.classList.toggle("selected");
