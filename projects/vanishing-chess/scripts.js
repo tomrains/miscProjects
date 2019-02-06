7a2c8d093ae538ef910b89f0031935bb928048b9
tomrains <tomrainswrites@gmail.com>
2019-02-05 21:05:22 -0600

    Add numbers to chessboard array
    There's probably a way to make ids that are more useful.

--- projects/vanishing-chess/scripts.js
+++ projects/vanishing-chess/scripts.js
@@ -1,11 +1,11 @@
-var chess_board = [["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""], 
-                   ["", "", "", "", "", "", "", ""]]; 
+var chess_board = [["1", "2", "3", "4", "5", "6", "7", "8"], 
+                   ["9", "10", "11", "12", "13", "14", "15", "16"], 
+                   ["17", "18", "19", "20", "21", "22", "23", "24"], 
+                   ["25", "26", "27", "28", "29", "30", "31", "32"], 
+                   ["33", "34", "35", "36", "37", "38", "39", "40"], 
+                   ["41", "42", "43", "44", "45", "46", "47", "48"], 
+                   ["49", "50", "51", "52", "53", "54", "55", "56"], 
+                   ["57", "58", "59", "60", "61", "62", "63", "64"]]; 
 
 var move = true;
 
@@ -161,6 +161,12 @@ function movePawn(selected, el) {
   
 function moveRook(selected, el) {
   return;
+  //for now, let's let it move anywhere on the same row or column
+  //later, we will block it if a piece is standing in its way
+  //this is where the array would be PERFECT. you just need [x][y], one of them to match the previous one
 }
 
 setBoard();
+
+/*had slight epiphany that basically you let every piece move ... you put the moves through a function ..
+to see if it's an illegal move. if it is, you tell it that it cant move - yay! */
