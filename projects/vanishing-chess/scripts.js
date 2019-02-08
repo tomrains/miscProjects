847edab631a6148b2f2b75ba38cfeb9609b5ac88
tomrains <tomrainswrites@gmail.com>
2019-02-08 17:42:39 -0600

    Add to stop if selected equal to null
    I should never have taken that bit out! I didn't understand what it meant, so I did! Lesson learned.

--- projects/vanishing-chess/scripts.js
+++ projects/vanishing-chess/scripts.js
@@ -93,7 +93,7 @@ function allowMove(el) {
   //find selected square
   var selected = document.querySelector("td.selected");
   //if selected equal to el, then don't try to move the piece
-  if (selected == el && selectedExists == true) {
+  if ((selected == el && selectedExists == true) || selected == null) {
     return;
   }
   if (selected.innerHTML == whitePawn || selected.innerHTML == blackPawn) {
