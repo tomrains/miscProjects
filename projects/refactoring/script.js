866d48e8737e1a6438220861d1075ddfca172dc9
tomrains <tomrainswrites@gmail.com>
2019-02-23 20:35:08 -0600

    Add pieces variables

--- projects/refactoring/script.js
+++ projects/refactoring/script.js
@@ -1,3 +1,18 @@
+//all the pieces
+var blackRook = '<img src="images/black-rook.png">';
+var blackKnight = '<img src="images/black-knight.png">';
+var blackBishop = '<img src="images/black-bishop.png">';
+var blackQueen = '<img src="images/black-queen.png">';
+var blackKing = '<img src="images/black-king.png">';
+var blackPawn = '<img src="images/black-pawn.png">';
+var whiteRook = '<img src="images/white-rook.png">';
+var whiteKnight = '<img src="images/white-knight.png">';
+var whiteBishop = '<img src="images/white-bishop.png">';
+var whiteQueen = '<img src="images/white-queen.png">';
+var whiteKing = '<img src="images/white-king.png">';
+var whitePawn = '<img src="images/white-pawn.png">';
+var blackHole = '<img src="images/black-hole.png">';
+
 //adding the necessary click events to each TD
 document.getElementsByTagName("TD").addEventListener("click", getMoving(this));
 
