a9ccf9d80447fd963b8debca861a13c337e8fe13
tomrains <tomrainswrites@gmail.com>
2019-04-12 09:41:11 -0700

    create new function for test squares
    this will do some WEIRD stuff, im sure. can tweak and test of course.

--- projects/conways-game-of-life/conway.js
+++ projects/conways-game-of-life/conway.js
@@ -44,3 +44,43 @@ function oneTurn() {
     squares[i].className = newClasses[i]; //this might just change the array and not the paragraps themselbes. we'll see
   }
 } //ending bracket for oneTurn function
+
+function testSquares() {
+  var classes = []; 
+  var newClasses = [];
+  var squares = document.getElementsByTagName('td');
+  for (i=0; i<squares.length; i++) {
+    classes[i] = squares[i].className;
+  }
+  for (z=0; z<classes.length; z++) {
+    if (z == 0) {
+      if (classes[z+1] == "alive") {
+        newClasses[z] = "alive";
+      }
+      else {
+        newClasses[z] = "";
+      }
+    } // last curly for if z == 0
+    else if (z > 0 && z < 7) {
+      if (classes[z - 1] == classes[z + 1]) {
+        newClasses[z] = "";
+      }
+      else {
+        newClasses[z] = "alive";
+      }
+    } // last curly for else if
+    else { //if z == 7
+      if (classes[z - 1] == "alive") {
+        newClasses[z] = "alive";
+      }
+      else {
+        newClasses[z] = "";
+      }
+    } // last curly for else
+  } //last curly for for
+  //below, im going to try looping through original node list to see if that helps
+  for (i=0; i<squares.length; i++) {
+    squares[i].className = newClasses[i]; //this might just change the array and not the paragraps themselbes. we'll see
+  }
+} //ending bracket for testSquares function
+
