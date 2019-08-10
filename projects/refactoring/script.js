ea2610805ac73436774cb2d21b92a8a6a06fdfe5
tomrains <tomrainswrites@gmail.com>
2019-08-10 09:51:41 -0500

    refactor a few elements
    swap out var, try to combine the event listener functions to add at once.

--- projects/refactoring/script.js
+++ projects/refactoring/script.js
@@ -1,14 +1,12 @@
 //we're going to start the JS code afresh!
 
 //add event listeners for selecting a piece and allowing a move
-var allSquares = document.getElementsByTagName("TD");
+const allSquares = document.getElementsByTagName("TD");
 for (let a = 0; a < allSquares.length; a++) {
   allSquares[a].addEventListener("click", 
     function() {
-      selectedPiece(allSquares[a]);
+      selectPiece(allSquares[a]);
     }
-  );
-  allSquares[a].addEventListener("click", 
     function() {
       allowMove(allSquares[a]);
     }
