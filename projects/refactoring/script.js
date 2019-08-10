c4cb99501840d0b545607c20a6ced389daa7741b
tomrains <tomrainswrites@gmail.com>
2019-08-10 10:41:01 -0500

    refactor selectedPiece function

--- projects/refactoring/script.js
+++ projects/refactoring/script.js
@@ -63,9 +63,48 @@ const blackHole = '<img src="images/black-hole.png">';
 const blackPieces = [blackPawn, blackRook, blackKnight, blackBishop, blackQueen, blackKing];
 const whitePieces = [whitePawn, whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing];
 
-function selectPiece(square) {
-  console.log("selectPiece");
-}
+function selectPiece(selectedPiece) {
+  //disallow moves from empty squares
+  if (el.innerHTML == "") {
+    return;
+  }
+  // find most previously selected piece (if there is one)
+  let lastSelectedPiece = document.querySelector("td.selected");
+  //disallow any pieces from capturing a king, but lets you toggle to move a king
+  if (lastSelectedPiece) {
+    if (blackPieces.indexOf(lastSelectedPiece.innerHTML) != -1 && selectedPiece.innerHTML == whiteKing) {
+      kingCapture = true; //remember to set this to false later
+      move = false;
+      return;
+    }
+    if (whitePieces.indexOf(lastSelectedPiece.innerHTML) != -1 && selectedPiece.innerHTML == blackKing) {
+      kingCapture = true; //remember to set this to false later
+      move = false;
+      return;
+    }
+  }
+  //return on capturing piece moves, so you can perform allowMove function
+  if (whitePieces.indexOf(selectedPiece.innerHTML) != -1 && blackPieces.indexOf(lastSelectedPiece.innerHTML) != -1) {
+    if (selectedPiece.innerHTML == whitePawn) {
+      whitePawnAttacking = true;
+    }
+    captureMove = true;
+    return;
+  }
+  if (blackPieces.indexOf(selectedPiece.innerHTML) != -1 && whitePieces.indexOf(lastSelectedPiece.innerHTML) != -1) {
+    if (selected.innerHTML == blackPawn) {
+      blackPawnAttacking = true;
+    }
+    captureMove = true;
+    return;
+  }
+  //add selected class to selectedPiece
+  selectedPiece.classList.toggle("selected");
+  //if whitesMove is true, and it's white piece, then let it toggle. same for black pieces, too
+  if ( (whitesMove && whitePieces.indexOf(lastSelectedPiece.innerHTML) != -1) || (!whitesMove && blackPieces.indexOf(lastSelectedPiece.innerHTML) != -1) ) {
+    lastSelectedPiece.classList.toggle("selected");
+  }
+} // last curly in selectedPiece function
 
 function allowMove(square) {
   console.log("allowMove");
