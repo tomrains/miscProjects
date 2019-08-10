a253dbe9977b1240e275956c22cebab3b263d3cf
tomrains <tomrainswrites@gmail.com>
2019-08-09 21:06:16 -0500

    add event listeners to select piece, allow move

--- projects/refactoring/script.js
+++ projects/refactoring/script.js
@@ -1,1 +1,16 @@
 //we're going to start the JS code afresh!
+
+//add event listeners for selecting a piece and allowing a move
+var allSquares = document.getElementsByTagName("TD");
+for (let a = 0; a < allSquares.length; a++) {
+  allSquares[a].addEventListener("click", 
+    function() {
+      selectedPiece(allSquares[a]);
+    }
+  );
+  allSquares[a].addEventListener("click", 
+    function() {
+      allowMove(allSquares[a]);
+    }
+  );
+}
