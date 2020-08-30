76f2960f1e27918394bb74b9545bc177e6b5bb19
tomrains <tomrainswrites@gmail.com>
2020-08-30 18:05:00 -0500

    add interHTML

--- projects/refactoring/script.js
+++ projects/refactoring/script.js
@@ -69,6 +69,7 @@ function selectPiece(selectedPiece) {
     return;
   }
   console.log(selectedPiece); // remove later
+  console.log(selectedPiece.innerHTML); // remove later
   // find most previously selected piece (if there is one)
   let lastSelectedPiece = document.querySelector("td.selected");
   //disallow any pieces from capturing a king, but lets you toggle to move a king
